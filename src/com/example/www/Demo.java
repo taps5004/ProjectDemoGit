32966ff565a06134e64da7e266c5c8cefcee2170
taps5004 <tanushreepattayat9495@gmail.com>
2025-03-17 09:49:36 +0530

    my wrk

--- src/com/example/www/Demo.java
+++ src/com/example/www/Demo.java
@@ -5,6 +5,11 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo");
+		int a = 10;
+		int b= 20;
+		int c= a+b;
+		System.out.println(+c);
+		
 	}
 
 }
---
+++ src/com/example1/www/Billing.java
@@ -0,0 +1,10 @@
+package com.example1.www;
+
+public class Billing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
